d4a1bdc4ad08e526e8b9b1664525ef837bd5b13a
z4kkyy <mathtaka1528@gmail.com>
2025-05-03 12:18:57 +0900

    Comment out unnecessary log statements

--- src/main/java/com/z4kkyy/chestsorter/BlockBreakEventHandler.java
+++ src/main/java/com/z4kkyy/chestsorter/BlockBreakEventHandler.java
@@ -3,13 +3,15 @@ package com.z4kkyy.chestsorter;
 import com.mojang.logging.LogUtils;
 import org.slf4j.Logger;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.Block;
+// import net.minecraft.resources.ResourceLocation;
+// import net.minecraft.world.item.Item;
+// import net.minecraft.world.item.ItemStack;
+// import net.minecraft.world.item.Items;
+// import net.minecraft.world.level.block.state.BlockState;
+// import net.minecraft.world.level.block.Block;
+// import net.minecraft.core.component.DataComponentMap;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.ForgeRegistries;
+// import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.event.level.BlockEvent;
 
 
@@ -19,16 +21,24 @@ public class BlockBreakEventHandler {
 
     @SubscribeEvent
     public void onBlockBreak(BlockEvent.BreakEvent event) {
-        BlockState state = event.getState();
-        Block block = state.getBlock();
-        Item blockItem = block.asItem();
+        // BlockState state = event.getState();
+        // Block block = state.getBlock();
+        // Item blockItem = block.asItem();
 
-        ResourceLocation regName = ForgeRegistries.ITEMS.getKey(blockItem);
+        // ItemStack blockItemStack = new ItemStack(blockItem);
+        // DataComponentMap blockProperties = blockItemStack.getComponents();
+        // if (blockProperties != null) {
+        //     LOGGER.info("Block properties: {}", blockProperties);
+        // } else {
+        //     LOGGER.info("No block properties found for item: {}", blockItem.getName().getString());
+        // }
 
-        if (blockItem == Items.AIR || regName == null) {
-            LOGGER.info("Broken block has no item form");
-        } else {
-            LOGGER.info("Broken block as item registry name: {}", regName);
-        }
+        // ResourceLocation regName = ForgeRegistries.ITEMS.getKey(blockItem);
+
+        // if (blockItem == Items.AIR || regName == null) {
+        //     LOGGER.info("Broken block has no item form");
+        // } else {
+        //     LOGGER.info("Broken block as item registry name: {}", regName);
+        // }
     }
 }
